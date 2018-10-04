ae676bee37fc38b3173624086f3517bd79bc9258
Samuel Leclerc <sam.h.leclerc@gmail.com>
2018-10-03 20:03:57 -0400

    First Commit

---
+++ TeensyPilot.BaseRadio/TeensyPilot.BaseRadio.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		TeensyPilot.ino
+ Created:	10/3/2018 8:01:26 PM
+ Author:	Sam
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
