614f218c617ba218b502e2f33f328a92569b49cf
Samuel Leclerc <sam.h.leclerc@gmail.com>
2018-10-03 20:00:09 -0400

    Add project files.

---
+++ TeensyPilot/TeensyPilot.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		TeensyPilot.ino
+ Created:	10/3/2018 7:59:56 PM
+ Author:	Sam
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
