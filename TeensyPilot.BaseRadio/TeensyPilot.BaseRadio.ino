64d0c829ec35330cf6a1025491b4d8c6eb0c1e6d
Samuel Leclerc <sam.h.leclerc@gmail.com>
2018-10-08 13:56:17 -0400

    First pass at transmiting IMU data

--- TeensyPilot.BaseRadio/TeensyPilot.BaseRadio.ino
+++ TeensyPilot.BaseRadio/TeensyPilot.BaseRadio.ino
@@ -1,15 +1,97 @@
-/*
- Name:		TeensyPilot.ino
- Created:	10/3/2018 8:01:26 PM
- Author:	Sam
-*/
+#include <RadioHead.h>
+#include <RH_RF95.h>
 
-// the setup function runs once when you press reset or power the board
-void setup() {
+// Serial debug
+#define SERIAL_DEBUG
 
+#ifdef SERIAL_DEBUG
+#define PLN(x) (Serial.println(x))
+#define P(x) (Serial.print(x))
+#endif
+
+#ifndef SERIAL_DEBUG
+#define PLN(x)
+#define P(x)
+#endif
+
+#define RFM95_CS 10
+#define RFM95_RST 9
+#define RFM95_INT 2
+
+// Change to 434.0 or other frequency, must match RX's freq!
+#define RF95_FREQ 434.0
+
+// Singleton instance of the radio driver
+RH_RF95 rf95(RFM95_CS, RFM95_INT);
+
+// Blinky on receipt
+#define LED 13
+
+// Data types
+union imuPacket
+{
+	float imuDat[5];
+	uint8_t bytes[20];
+};
+
+void setup()
+{
+	pinMode(LED, OUTPUT);
+	pinMode(RFM95_RST, OUTPUT);
+	digitalWrite(RFM95_RST, HIGH);
+
+	while (!Serial);
+	Serial.begin(115200);
+	delay(100);
+
+	while (!rf95.init()) {
+		Serial.println("LoRa radio init failed");
+		while (1);
+	}
+	Serial.println("LoRa radio init OK!");
+
+	// Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM
+	if (!rf95.setFrequency(RF95_FREQ)) {
+		Serial.println("setFrequency failed");
+		while (1);
+	}
+	Serial.print("Set Freq to: "); Serial.println(RF95_FREQ);
+
+	// Defaults after init are 434.0MHz, 13dBm, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on
+
+	// The default transmitter power is 13dBm, using PA_BOOST.
+	// If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then
+	// you can set transmitter powers from 5 to 23 dBm:
+	rf95.setTxPower(23, false);
 }
 
-// the loop function runs over and over again until power down or reset
-void loop() {
-  
+void loop()
+{
+	if (rf95.available())
+	{
+		// Should be a message for us now
+		imuPacket buf;
+		uint8_t len = sizeof(buf);
+		memset(buf.bytes, 0, sizeof buf);
+
+		if (rf95.recv(buf.bytes, &len))
+		{
+			digitalWrite(LED, HIGH);
+
+			P(F("H:"));
+			P(buf.imuDat[0]);
+			P(F(" P:"));
+			P(buf.imuDat[1]);
+			P(F(" R:"));
+			P(buf.imuDat[2]);
+			P(F(" T:"));
+			P(buf.imuDat[3]);
+			P(F(" A:"));
+			PLN(buf.imuDat[4]);
+		}
+		else
+		{
+			Serial.println("Receive failed");
+		}
+	}
 }
--- TeensyPilot/TeensyPilot.ino
+++ TeensyPilot/TeensyPilot.ino
@@ -1,15 +1,242 @@
-/*
- Name:		TeensyPilot.ino
- Created:	10/3/2018 7:59:56 PM
- Author:	Sam
-*/
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_LSM303_U.h>
+#include <Adafruit_BMP085_U.h>
+#include <Adafruit_L3GD20_U.h>
+#include <Adafruit_10DOF.h>
+#include <SPI.h>
+#include <RH_RF95.h>
+#include <MahonyAHRS.h>
 
-// the setup function runs once when you press reset or power the board
-void setup() {
+// Serial debug
+//#define SERIAL_DEBUG
 
+#ifdef SERIAL_DEBUG
+#define PLN(x) (Serial.println(x))
+#define P(x) (Serial.print(x))
+#endif
+
+#ifndef SERIAL_DEBUG
+#define PLN(x)
+#define P(x)
+#endif
+
+// Radio
+// Pins
+#define RFM95_CS 10
+#define RFM95_RST 24
+#define RFM95_INT 2
+
+#define RF95_FREQ 434.0
+
+RH_RF95 rf95(RFM95_CS, RFM95_INT);
+
+// IMU
+#define SAMP_PER_SEC 70
+
+float mag_offsets[3] = { 30.05F, -3.03F, -11.19F };
+
+float mag_softiron_matrix[3][3] = { { 1.032, -0.05, -0.025 },
+									{ -0.05, 0.959, 0.0 },
+									{ -0.025, 0.02, 1.014 } };
+
+float mag_field_strength = 66.45F;
+
+Adafruit_L3GD20_Unified       gyro(20);
+Adafruit_LSM303_Accel_Unified accel(30301);
+Adafruit_LSM303_Mag_Unified   mag(30302);
+Adafruit_BMP085_Unified       bmp(18001);
+
+Mahony filter;
+
+// Data types
+union imuPacket
+{
+	float imuDat[5];
+	uint8_t bytes[20];
+};
+
+void setup()
+{
+	// Stabilize
+	delay(100);
+
+#ifdef SERIAL_DEBUG
+	while (!Serial);
+	Serial.begin(115200);
+	delay(100);
+#endif
+
+	InitRadio();
+	InitSensors();
+
+	// Stabilize
+	delay(100);
 }
 
-// the loop function runs over and over again until power down or reset
-void loop() {
-  
+void loop()
+{
+	imuPacket imu;
+	GetIMUData(imu.imuDat);
+
+	Transmit(imu.bytes, 20);
+}
+
+void InitRadio()
+{
+	while (!rf95.init())
+	{
+		PLN(F("LoRa radio init failed"));
+		while (1);
+	}
+	PLN(F("LoRa radio init OK"));
+
+	if (!rf95.setFrequency(RF95_FREQ)) {
+		PLN(F("setFrequency failed"));
+		while (1);
+	}
+	P(F("Set Freq to: "));
+	PLN(RF95_FREQ);
+
+	rf95.setTxPower(23);
+}
+
+void InitSensors()
+{
+	if (!accel.begin())
+	{
+		PLN(F("No LSM303 (accel) detected"));
+		while (1);
+	}
+	PLN(F("LSM303 (accel) init OK"));
+
+	if (!gyro.begin())
+	{
+		PLN(F("No L3GD20 (gyro) detected"));
+		while (1);
+	}
+	PLN(F("L3GD20 (gyro) init OK"));
+
+	if (!mag.begin())
+	{
+		PLN(F("No LSM303 (mag) detected"));
+		while (1);
+	}
+	PLN(F("LSM303 (mag) init OK"));
+
+	if (!bmp.begin())
+	{
+		PLN(F("No BMP180 (baro) detected"));
+		while (1);
+	}
+	PLN(F("BMP180 (baro) init OK"));
+
+	filter.begin(SAMP_PER_SEC);
+}
+
+void Transmit(uint8_t *radiopacket, int size)
+{
+	P(F("Sending: "));
+	PLN((char*)radiopacket);
+	if (rf95.send((uint8_t *)radiopacket, size))
+	{
+		PLN(F("Success, queued for transmission"));
+	}
+	else
+	{
+		PLN(F("Failure, CAD timeout"));
+	}
+}
+
+// TODO Watch out for buf, could be empty when returned. Look at malloc. Don't forget to free
+uint8_t* Receive()
+{
+	uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
+	uint8_t len = sizeof(buf);
+
+	PLN(F("Waiting for reply: "));
+	if (rf95.waitAvailableTimeout(10))
+	{
+		if (rf95.recv(buf, &len))
+		{
+			PLN(F("Got reply: "));
+			PLN((char*)buf);
+			PLN(F("RSSI: "));
+			PLN(rf95.lastRssi());
+			return buf;
+		}
+		else
+		{
+			PLN(F("Receive failed"));
+		}
+	}
+	else
+	{
+		PLN(F("WaitAvailableTimeout() Timed out"));
+	}
+}
+
+void GetIMUData(float *dat)
+{
+	sensors_event_t gyro_event;
+	sensors_event_t accel_event;
+	sensors_event_t mag_event;
+	sensors_event_t bmp_event;
+
+	// Get new data samples
+	gyro.getEvent(&gyro_event);
+	accel.getEvent(&accel_event);
+	mag.getEvent(&mag_event);
+	bmp.getEvent(&bmp_event);
+
+	// Apply mag offset compensation (base values in uTesla)
+	float x = mag_event.magnetic.x - mag_offsets[0];
+	float y = mag_event.magnetic.y - mag_offsets[1];
+	float z = mag_event.magnetic.z - mag_offsets[2];
+
+	// Apply mag soft iron error compensation
+	float mx = x * mag_softiron_matrix[0][0] + y * mag_softiron_matrix[0][1] + z * mag_softiron_matrix[0][2];
+	float my = x * mag_softiron_matrix[1][0] + y * mag_softiron_matrix[1][1] + z * mag_softiron_matrix[1][2];
+	float mz = x * mag_softiron_matrix[2][0] + y * mag_softiron_matrix[2][1] + z * mag_softiron_matrix[2][2];
+
+	// rad/s to deg/s
+	float gx = gyro_event.gyro.x * 57.2958F;
+	float gy = gyro_event.gyro.y * 57.2958F;
+	float gz = gyro_event.gyro.z * 57.2958F;
+
+	// Update the filter
+	filter.update(gx, gy, gz,
+		accel_event.acceleration.x, accel_event.acceleration.y, accel_event.acceleration.z,
+		mx, my, mz);
+
+	float temperature, altitude;
+	if (bmp_event.pressure)
+	{
+		bmp.getTemperature(&temperature);
+		altitude = bmp.pressureToAltitude(SENSORS_PRESSURE_SEALEVELHPA,
+			bmp_event.pressure,
+			temperature);
+	}
+
+	float roll = filter.getRoll();
+	float pitch = filter.getPitch();
+	float heading = filter.getYaw();
+
+	dat[0] = heading;
+	dat[1] = pitch;
+	dat[2] = roll;
+	dat[3] = temperature;
+	dat[4] = altitude;
+
+	P(millis());
+	P(F(" - H:"));
+	P(heading);
+	P(F(" P:"));
+	P(pitch);
+	P(F(" R:"));
+	P(roll);
+	P(F(" T:"));
+	P(temperature);
+	P(F(" A:"));
+	PLN(altitude);
 }
